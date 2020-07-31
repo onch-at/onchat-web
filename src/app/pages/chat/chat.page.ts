829f0ecd56977b4e9a24e0e90f0612daf7120a10
e <1838491745@qq.com>
2020-07-31 23:26:26 +0800

    fix: 修复在聊天页面跳转聊天页面，消息内容同时存在于两个页面的问题

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,9 +1,9 @@
 import { KeyValue } from '@angular/common';
 import { Component, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, Scroll } from '@angular/router';
 import { IonContent } from '@ionic/angular';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { filter, takeUntil } from 'rxjs/operators';
 import { ChatroomType, MessageType, SocketEvent } from 'src/app/common/enum';
 import { StrUtil } from 'src/app/common/utils/str.util';
 import { SysUtil } from 'src/app/common/utils/sys.util';
@@ -25,6 +25,8 @@ export class ChatPage implements OnInit {
   msg: string = '';
   /** 当前房间名字 */
   roomName: string = '';
+  /** 房间ID */
+  chatroomId: number;
   /** 消息ID，用于查询指定消息段 */
   msgId: number = 0;
   /** 聊天记录 */
@@ -56,18 +58,29 @@ export class ChatPage implements OnInit {
   ) { }
 
   ngOnInit() {
+    // 监听路由 滚动事件
+    this.router.events.pipe(filter(event => event instanceof Scroll), takeUntil(this.subject)).subscribe((event: Scroll) => {
+      // 尝试从URL中提取chatroomId
+      const chatroomId = +event.routerEvent.url.replace(/\/chat\//, '');
+      // 如果提取到的是一个数字，并且服务中的chatroomId跟这个chatroomId不一样，则更新
+      if (!isNaN(chatroomId) && this.onChatService.chatroomId != chatroomId) {
+        this.onChatService.chatroomId = chatroomId;
+      }
+    });
+
     // 记录当前房间ID，用于处理聊天列表
     this.onChatService.chatroomId = this.route.snapshot.params.id;
+    this.chatroomId = this.route.snapshot.params.id;
 
     this.loadRecords();
 
     // 先去聊天列表缓存里面查，看看有没有这个房间的数据
-    const index = this.onChatService.chatList.findIndex((v: ChatItem) => v.chatroomId == this.onChatService.chatroomId);
+    const index = this.onChatService.chatList.findIndex((v: ChatItem) => v.chatroomId == this.chatroomId);
     if (index >= 0) {
       this.roomName = this.onChatService.chatList[index].name;
       this.chatroomType = this.onChatService.chatList[index].type;
     } else { // TODO 把数据缓存下来
-      this.onChatService.getChatroom(this.onChatService.chatroomId).subscribe((result: Result<Chatroom>) => {
+      this.onChatService.getChatroom(this.chatroomId).subscribe((result: Result<Chatroom>) => {
         if (result.code === 0) {
           const chatroom = result.data
           this.roomName = chatroom.name;
@@ -78,7 +91,7 @@ export class ChatPage implements OnInit {
 
     this.socketService.on(SocketEvent.Message).pipe(takeUntil(this.subject)).subscribe((o: Result<Message>) => {
       // 如果请求成功，并且收到的消息是这个房间的
-      if (o.code == 0 && o.data.chatroomId == this.onChatService.chatroomId) {
+      if (o.code == 0 && o.data.chatroomId == this.chatroomId) {
         const canScrollToBottom = this.contentElement.scrollHeight - this.contentElement.scrollTop - this.contentElement.clientHeight <= 50;
 
         this.msgList.push(o.data);
@@ -89,13 +102,13 @@ export class ChatPage implements OnInit {
           this.hasUnread = true;
         }
         // 如果消息不是自己的，就设为已读
-        o.data.userId != this.onChatService.userId && this.onChatService.readed(this.onChatService.chatroomId).subscribe();
+        o.data.userId != this.onChatService.userId && this.onChatService.readed(this.chatroomId).subscribe();
       }
     });
 
     this.socketService.on(SocketEvent.RevokeMsg).pipe(takeUntil(this.subject)).subscribe((o: Result<{ chatroomId: number, msgId: number }>) => {
       // 如果请求成功，并且收到的消息是这个房间的
-      if (o.code == 0 && o.data.chatroomId == this.onChatService.chatroomId) {
+      if (o.code == 0 && o.data.chatroomId == this.chatroomId) {
         for (const msgItem of this.msgList) {
           if (msgItem.id == o.data.msgId) { // 移除被撤回的那条消息
             msgItem.type = MessageType.Tips;
@@ -109,7 +122,7 @@ export class ChatPage implements OnInit {
   }
 
   ngOnDestroy() {
-    this.onChatService.chatroomId = null;
+    this.chatroomId = null;
     this.subject.next();
     this.subject.complete();
   }
@@ -185,7 +198,7 @@ export class ChatPage implements OnInit {
   loadRecords(complete?: CallableFunction) {
     if (this.end) { return complete && complete(); }
 
-    this.onChatService.getChatRecords(this.onChatService.chatroomId, this.msgId).subscribe((result: Result<Message[]>) => {
+    this.onChatService.getChatRecords(this.chatroomId, this.msgId).subscribe((result: Result<Message[]>) => {
       if (result.code === 0) {
         // 按照ID排序
         // result.data.sort((a: Message, b: Message) => {
@@ -242,7 +255,7 @@ export class ChatPage implements OnInit {
    */
   send(textareaElement: HTMLTextAreaElement) {
     if (this.msg.length > MSG_MAX_LENGTH) { return; }
-    const msg = new Message(+this.onChatService.chatroomId);
+    const msg = new Message(+this.chatroomId);
     // TODO 封装成一个文字消息类
     msg.data = this.msg;
     this.socketService.message(msg);
@@ -278,10 +291,10 @@ export class ChatPage implements OnInit {
         }
       }
     ], (data: KeyValue<string, any>) => {
-      this.onChatService.setFriendAlias(this.onChatService.chatroomId, data['alias']).subscribe((result: Result) => {
+      this.onChatService.setFriendAlias(this.chatroomId, data['alias']).subscribe((result: Result) => {
         if (result.code == 0) {
           this.roomName = data['alias'];
-          const index = this.onChatService.chatList.findIndex((v: ChatItem) => v.chatroomId == this.onChatService.chatroomId);
+          const index = this.onChatService.chatList.findIndex((v: ChatItem) => v.chatroomId == this.chatroomId);
           if (index >= 0) {
             this.onChatService.chatList[index].name = data['alias'];
             this.onChatService.chatList = this.onChatService.chatList;
