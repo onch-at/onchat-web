0828177eb5285719bcb38917cab327ba14c36048
HyperLife1119 <hyperlife1119@qq.com>
2021-10-07 22:49:38 +0800

    chore(overlay): remove the tag config from NotificationOptions

--- src/app/services/overlay.service.ts
+++ src/app/services/overlay.service.ts
@@ -52,7 +52,6 @@ export class Overlay {
     if ('Notification' in window && Notification.permission === 'granted') {
       const { title, description, icon, url } = options;
       this.navigator.serviceWorker.ready.then((registration: ServiceWorkerRegistration) => registration.showNotification(title, {
-        tag: title,
         body: description,
         badge: '/assets/icon/favicon.ico',
         icon,
